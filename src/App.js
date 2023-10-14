8a707a731abc8ceef8622f3352c5fe2556586594
me4abhi <abhinavsaini669@gmail.com>
2023-10-14 16:54:33 +0530

    CRUD Finished

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-import './App.css';
-import React, { Component } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Container from 'react-bootstrap/Container';
-import InvoiceForm from './components/InvoiceForm';
+import "./App.css";
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import InvoiceForm from "./components/InvoiceForm";
+import InvoiceData from "./components/InvoiceData";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 class App extends Component {
   render() {
-  return (
-    <div className="App d-flex flex-column align-items-center justify-content-center w-100">
-      <Container>
-        <InvoiceForm/>
-      </Container>
-    </div>
-  );
-}}
+    return (
+      <BrowserRouter>
+        <div className="App d-flex flex-column align-items-center justify-content-start w-100 mt-5">
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={InvoiceData} />
+              <Route path="/new-invoice" component={InvoiceForm} />
+            </Switch>
+          </div>
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
 
 export default App;
